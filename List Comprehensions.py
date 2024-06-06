7150210b065ef202bc5192f0e981005835ea8bbf
Badal Singh Kushwaha <63418727+badal1509@users.noreply.github.com>
2024-06-06 23:17:41 +0530

    _

---
+++ List Comprehensions.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+print("[",end="")
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if i+j+k!=n:
+                if i+j+k==x+y+z:
+                    print(f"[{i}, {j}, {k}]",end="")
+                    break
+                print(f"[{i}, {j}, {k}]",end=", ")
+print("]")
