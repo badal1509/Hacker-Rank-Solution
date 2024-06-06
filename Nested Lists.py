45956eedf7b91ff0fda71c281c6d2c3f740ab82f
Badal Singh Kushwaha <63418727+badal1509@users.noreply.github.com>
2024-06-06 23:19:47 +0530

    _

---
+++ Nested Lists.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    record=[]
+    lis2=[]
+    nam=[]
+    rd=[]
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        record.append([name,score])
+        lis2.append(score)
+    lis2.sort()
+    for i in lis2:
+        if i not in rd:
+            rd.append(i)
+    sl=rd[1]
+    for n,s in record:
+        if s==sl:
+            nam.append(n)
+    nam.sort()
+    for i in nam:
+        print(i)
