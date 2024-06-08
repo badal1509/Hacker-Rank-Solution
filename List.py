da6f85851774705a4011dec91184c1765db62b0e
Badal Singh Kushwaha <63418727+badal1509@users.noreply.github.com>
2024-06-08 22:50:41 +0530

    Update List.py

--- List.py
+++ List.py
@@ -3,13 +3,10 @@ if __name__ == '__main__':
     li = []
     for i in range(0, N):
         cmd = list(input().split())
-
         if (cmd[0] == "insert"):
             li.insert(int(cmd[1]), int(cmd[2]))
-
         if (cmd[0] == "print"):
             print(li)
-
         if (cmd[0] == "remove"):
             li.remove(int(cmd[1]))
         if (cmd[0] == "append"):
