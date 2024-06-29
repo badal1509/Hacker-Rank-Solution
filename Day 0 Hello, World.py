af0af9bfb61ab68d7f1c464d8578a3c5e8a02ca2
Badal Singh Kushwaha <63418727+badal1509@users.noreply.github.com>
2024-06-29 18:26:29 +0530

    -

--- Day 0 Hello, World.py
+++ Day 0 Hello, World.py
@@ -2,6 +2,6 @@
 input_string = input()
 
 # Print a string literal saying "Hello, World." to stdout.
-print('Hello, World. this is me Badal Singh')
+print('Hello, World. this is me Badal Singh Kushwaha')
 
 # TODO: Write a line of code here that prints the contents of input_string to stdout.
