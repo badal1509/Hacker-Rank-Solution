87132a4c4f982d23b978a6d8205cb5840c554c2d
Badal Singh Kushwaha <63418727+badal1509@users.noreply.github.com>
2024-08-14 10:28:31 +0530

    -

--- Alphabet Rangoli.py
+++ Alphabet Rangoli.py
@@ -1,5 +1,5 @@
 def print_rangoli(size):
-    alpha = "abcdefghijklmnopqrstuvwxyz"
+    alpha = "abcdefghijklmnopqrstuvwxyz "
     data = [alpha[i] for i in range(size)]
     items = list(range(size))
     items = items[:-1]+items[::-1]
